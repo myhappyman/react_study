6b9456dd60a0a491991bcdc560c630d15b63d253
ParkShinwoo <myhappyman@naver.com>
2022-10-04 17:12:26 +0900

    useEffect를 통해 페이지로드 후  한번만 동작하도록 설정을 할 수 있다.

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,21 @@
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = ()=> setCounter(cur => cur + 1);
+  console.log("App called");
+  const iRunOnlyOnce = () => {
+    console.log("App i Run only once");
+  }
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1 className={styles.title}>Welcome back!</h1>
+      <p>{counter}</p>
       <Button text="Continue" />
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
