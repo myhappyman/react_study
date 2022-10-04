91869b03139d8294a6d42f530f9033d103fefe17
ParkShinwoo <myhappyman@naver.com>
2022-10-04 17:33:34 +0900

    useEffect를 통해 한번만 동작시킬지 특정 state에 변화가 있을때마다 동작시킬지 등 감시를 통해 제어를 할 수 있게 되었다.

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,17 +4,32 @@ import styles from "./App.module.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = ()=> setCounter(cur => cur + 1);
-  console.log("App called");
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter(cur => cur + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  // console.log("App called");
   const iRunOnlyOnce = () => {
-    console.log("App i Run only once");
+    console.log("I run only once.");
   }
-  useEffect(iRunOnlyOnce, []);
+  useEffect(iRunOnlyOnce, []); //페이지가 로드되었을때 iRunOnlyOnce라는 함수만 한번 동작한다.
+  useEffect(()=>{ //keyword state에 변화가 발생할때만 동작한다.
+    if(keyword !== "" && keyword.length > 6){
+      console.log("I run when 'keyword' changes");
+    }
+  }, [keyword]);
+  useEffect(()=>{ //keyword state에 변화가 발생할때만 동작한다.
+    console.log("I run when 'counter' changes");
+  }, [counter]);
+
+  useEffect(()=>{ //keyword, counter state에 변화가 발생하면 동작한다.
+    console.log("I run when 'counter' & 'keyword' changes");
+  }, [counter, keyword]);
   return (
     <div>
+      <input type="text" onChange={onChange} value={keyword} placeholder="Search here..." />
       <h1 className={styles.title}>Welcome back!</h1>
       <p>{counter}</p>
-      <Button text="Continue" />
+      
       <button onClick={onClick}>Click me</button>
     </div>
   );
